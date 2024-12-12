e13e121afce9cc967d1a557648cad6e2330446ce
priyadwivedi1701 <priyadwivedi1701@gmail.com>
2024-12-12 11:43:02 +0530

    Create CapInString.java

---
+++ CapInString.java
@@ -0,0 +1,15 @@
+public class temp2 {
+    public static void main(String[] args) {
+        String a="AbnDef";
+        int count=0;
+        for(int i=0;i<a.length();i++){
+            char c=a.charAt(i);
+        if(c>=65 && c<=90){
+            count++;
+            
+        }
+           
+    } System.out.println("There are "+count+"capital letter");
+}
+    
+}
